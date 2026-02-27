71f96f53a0d10e6fd7298825120f2ddb96f5a644
Emiliofb <emilioferba@hotmail.com>
2026-02-27 01:33:50 +0100

    add github pages config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/content', '@nuxt/image', '@nuxt/icon'],
-  css: ['@/assets/scss/main.scss']
+  css: ['@/assets/scss/main.scss'],
+  ssr: false, // obligatorio para GitHub Pages
+  nitro: {
+    preset: 'github_pages'
+  },
+  app: {
+    baseURL: '/MyBoardGames/' // 👈 MUY IMPORTANTE
+  }
 })
